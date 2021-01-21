f0cedefbfed864225f830f94b642a137cdc31e75
stopdaydreaming <hello@stopdaydreaming.com>
2021-01-21 17:59:30 -0500

    updated sql and updated functions

--- index.js
+++ index.js
@@ -12,33 +12,92 @@ const connection = mysql.createConnection({
 
   // Your password
   password: "HotTo$$y",
-  database: "greatBayDB"
+  database: "greatBayDB",
 });
 
-connection.connect(function(err) {
+connection.connect((err) => {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-  createProduct();
+  console.log(`connected as id: ${connection.threadId}`);
+  init();
 });
 
-function postAuctionItem() {
+function bidOnItem() {
+  connection.query("SELECT * FROM items", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+    const arrayOfItems = data.map((item) => {
+      return { name: item.name, value: item.id };
+    });
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "selection",
+          message: "Which item would you like to bid on?",
+          choices: arrayOfItems,
+        },
+        {
+          type: "input",
+          name: "newBid",
+          message: "How much would you like to bid?",
+        },
+      ])
+      .then(({ newBid, selection }) => {
+        console.log(selection);
+        // console.log(newBid);
+        // 1. See if the user's bid is higher than the current bid.
+        for (let i = 0; i < data.length; i++) {
+          if (data[i].id === selection) {
+            console.log("Current Bid: ", data[i].bid);
+            console.log("Your Bid: ", newBid);
+            if (data[i].bid < newBid) {
+              // TODO: Update the item.
+              console.log("Your bid is good!");
+              connection.query(
+                `UPDATE items SET bid = ? WHERE id = ?;`,
+                [newBid, selection],
+                (err, data) => {
+                  if (err) throw err;
+                  console.log("Your bid was successfully registered!");
+                  init();
+                }
+              );
+            } else {
+              // TODO: Alert the user that their bid is not sufficient.
+              console.log("Your bid is too low!");
+              init();
+            }
+            break;
+          }
+        }
+      });
+  });
+  // 1. Retrieve a list of items from the database via SQL query
+  // 2. Inject the list of items into inquirer as options
+  // 3. Call inquirer.prompt
+  // 4. Accept the user's input
+  // 5. ???
+}
+
+function postItemForAuction() {
   inquirer
     .prompt([
       {
         name: "name",
-        message: "item name",
-        type: "input"
+        message: "What is the item's name?",
+        type: "input",
       },
       {
         name: "bid",
-        message: "bid price",
-        type: "input"
-      }
+        message: "What is the item's starting bid?",
+        type: "input",
+      },
     ])
-    .them(({ name, bid }) => {
+    .then(({ name, bid }) => {
       connection.query(
         `INSERT INTO items (name, bid)
-            VALUES ("football", 10);`,
+        VALUES (?, ?);`,
+        [name, bid],
         (err, data) => {
           if (err) throw err;
           console.log(data);
@@ -49,15 +108,32 @@ function postAuctionItem() {
 }
 
 function init() {
-  //prompts
+  inquirer
+    .prompt([
+      {
+        name: "action",
+        message: "What would you like to do?",
+        type: "list",
+        choices: ["ADD AN ITEM FOR AUCTION", "BID ON ITEM", "EXIT"],
+      },
+    ])
+    .then(({ action }) => {
+      if (action === "ADD AN ITEM FOR AUCTION") {
+        postItemForAuction();
+      } else if (action === "BID ON ITEM") {
+        bidOnItem();
+      } else {
+        exit();
+      }
+    });
 }
 
 function exit() {
   connection.end();
 }
-// 1 hard code queries in mysql workbench
-// 2-bring queries in and add into functions
-// 3-test each function individually
-// 4-incorporate inquirer prompt to call these functions
-// 5-add additional inquirer prompts to take user input
-// 6-modify the sql queries to be dynamic
+// 1. Hard code queries in MySQL Workbench
+// 2. Bring queries in and add into functions.
+// 3. Test each function individually.
+// 4. Incorporate inquirer.prompt to call these functions.
+// 5. Add additional inquirer prompts to take in user input.
+// 6. Modify the SQL queries to be dynamic.
